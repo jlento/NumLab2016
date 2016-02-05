a29630e729216ec3b3ced4b72d7dd4f62fd3285c
Juha Lento <juha.lento@csc.fi>
2016-02-05 11:01:39 +0200

    Minor update

--- scripts/oifs_run_acceptance_taito.bash
+++ scripts/oifs_run_acceptance_taito.bash
@@ -5,11 +5,15 @@ nthreads=2
 rtime=10
 partition=test
 
+# Set up input files
+
 sdir=${USERAPPL}/oifs/src
 ver=oifs38r1
 cd ${WRKDIR}
 tar --strip-components=1 -x -f ${sdir}/${ver}.t*gz ${ver}/t21test
 cd t21test
+
+# Modify fort.4 namelist
 
 fixfort4() {
     local name value prog=""
@@ -24,7 +28,11 @@ fixfort4() {
 export -f fixfort4
 fixfort4 NPROC=${ntasks} LREFOUT=true NSTOP=144
 
+# Se the number OpenMP threads
+
 export OMP_NUM_THREADS=${nthreads}
+
+# Create a batch job script and submit it
 
 sbatch -n ${ntasks} -c ${nthreads} -t ${rtime} -p ${partition} <<EOF
 #!/bin/bash
